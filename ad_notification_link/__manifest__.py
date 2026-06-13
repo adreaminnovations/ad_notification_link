003f15a1abe948dd9bfb0758005bec2ad19b4edf
adreaminnovations <adreaminnovations@gmail.com>
2026-06-13 11:39:57 +0530

    [ADD] Added module

--- ad_notification_link/__manifest__.py
+++ ad_notification_link/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Notification Link',
-    'version': '18.0.1.0',
+    'version': '19.0.1.0',
     'category': 'Mail',
     'website': 'https://adreaminnovations.odoo.com',
     'author': 'ADream Innovations',
